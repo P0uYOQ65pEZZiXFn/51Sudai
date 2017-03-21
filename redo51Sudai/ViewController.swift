02f24cf42da8783ae56260de9600fad3419ad598
P0uYOQ65pEZZiXFn <zhongliangliang@ZhongLiangLiangdeMacBook-Pro.local>
2017-03-21 10:12:32 +0800

    nono

--- redo51Sudai/ViewController.swift
+++ redo51Sudai/ViewController.swift
@@ -16,9 +16,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func click(_ sender: Any) {
-        print("\(Config.shareInstance.imageUrl),\(Config.shareInstance.serverUrl)")
+        print("\(Config.shareInstance.imageUrl)---\(Config.shareInstance.serverUrl)")
         HUD.showError(string: "网络错误", vc: self)
-        print("\(Config_DOM.shareInstance.imageUrl)")
+        print("\(Config_DOM.shareInstance.imageUrl)---\(Config_DOM.shareInstance.serverUrl)")
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
